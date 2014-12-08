ce7d2d8d86f257d0683195192e5eb844bc12d2ac
Salimane Adjao Moustapha <me@salimane.com>
2014-12-08 01:33:30 +0100

    move .com to io

---
+++ media/js/bottom-js.js
@@ -0,0 +1,11 @@
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-28010526-1']);
+_gaq.push(['_setDomainName', 'salimane.com']);
+_gaq.push(['_trackPageview']);
+      
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
---
+++ static/js/top-js.js
@@ -0,0 +1,5 @@
+function write_email() {
+	document.write([">", "a", "/", "<", "m", "o", "c", ".", "e", "n", "a", "m", "i", "l", "a", "s", "@", "e", "m", ">", "\"", "m", "o", "c", ".", "e", "n", "a", "m", "i", "l", "a", "s", "@", "e", "m", ":", "o", "t", "l", "i", "a", "m", "\"", "=", "f", "e", "r", "h", " ", "a", "<"].reverse().join(''));
+}
+
+function delayer() {window.location = "/";}
